f43928f6ad75c072d2b8223dee12f76c2b875505
71405245 <71405245@continental.edu.pe>
2026-05-18 18:14:30 -0500

    Borre el antiguo programa PYTHON/DJANGO

--- SIMA/legacy_django/usuarios/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Usuario
-
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('username', 'rol', 'codigo', 'ciclo_actual', 'creditos_acumulados')
-    list_filter = ('rol',)
-    search_fields = ('username', 'codigo')
-
-admin.site.register(Usuario, UsuarioAdmin)
